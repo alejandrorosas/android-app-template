006d6dea238c9be93a2f58c24f7ad5ef61b8595a
Alejandro Rosas <alejandrorosasdev@gmail.com>
2022-04-24 13:42:39 +0200

    Upgrades and build time improvements

--- features/home/src/main/java/dev/alejandrorosas/home/ui/Home.kt
+++ features/home/src/main/java/dev/alejandrorosas/home/ui/Home.kt
@@ -15,8 +15,8 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import dev.alejandrorosas.core.R
 import dev.alejandrorosas.core.ui.AppTheme
-import dev.alejandrorosas.home.R
 
 @Composable
 fun Home(navController: NavController) {

12e57a2a0c2c279a401a3a77d9e3ac5095f77248
Alejandro Rosas <alejandrorosasdev@gmail.com>
2025-03-09 15:26:51 +0100

    Add Compose Screenshot Testing

---
+++ features/home/src/screenshotTest/kotlin/dev/alejandrorosas/home/HomePreviewsScreenshots.kt
@@ -0,0 +1,13 @@
+package dev.alejandrorosas.home
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import dev.alejandrorosas.home.ui.Screen
+
+class HomePreviewsScreenshots {
+    @Preview
+    @Composable
+    private fun HomePreview() {
+        Screen(onButtonClick = {})
+    }
+}

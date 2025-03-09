1c32e142d364d697e40ca72c77f04c002f921319
Alejandro Rosas <alejandrorosasdev@gmail.com>
2025-03-09 15:42:28 +0100

    Preview for Dark Mode

--- features/home/src/screenshotTest/kotlin/dev/alejandrorosas/home/HomePreviewsScreenshots.kt
+++ features/home/src/screenshotTest/kotlin/dev/alejandrorosas/home/HomePreviewsScreenshots.kt
@@ -1,5 +1,6 @@
 package dev.alejandrorosas.home
 
+import android.content.res.Configuration
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import dev.alejandrorosas.home.ui.Screen
@@ -8,6 +9,12 @@ class HomePreviewsScreenshots {
     @Preview
     @Composable
     private fun HomePreview() {
+        Screen(onButtonClick = {})
+    }
+
+    @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+    @Composable
+    private fun HomePreviewDarkMode() {
         Screen(onButtonClick = {})
     }
 }
